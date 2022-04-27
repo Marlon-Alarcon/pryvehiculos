7634e5ad55fd6ad072991593fc6f5dc47ae60f4d
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2022-04-26 22:38:30 -0500

    Ultima actividad - Commit

--- apps/vehiculos/admin.py
+++ apps/vehiculos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from apps.vehiculos.models import Marca,TipoVehiculo,Vehiculo
 
 # Register your models here.
+
+
+class Vehiculoadmin(admin.ModelAdmin):
+    list_display = ('modelo', 'color', 'placa', 'marca',"tipovehiculo")
+    ordering = ('modelo', 'color', 'placa', 'marca','tipovehiculo')
+    search_fields = ('modelo','marca__nombre', 'tipovehiculo__nombre')
+    list_filter = ('modelo','marca__nombre', 'tipovehiculo__nombre')
+
+admin.site.register(TipoVehiculo)
+admin.site.register(Marca)
+admin.site.register(Vehiculo, Vehiculoadmin)
--- apps/ventas/models.py
+++ apps/ventas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from apps.vehiculos.models import Vehiculo
 
 # Create your models here.
 
@@ -8,3 +9,14 @@ class Venta(models.Model):
     valorTotal = models.BigIntegerField()
     tipoPago = models.CharField(max_length=20)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    vehiculo = models.ManyToManyField(Vehiculo, through= 'VehiculoVenta')
+
+    def __str__(self):
+        return str(self.valorTotal)
+
+class VehiculoVenta(models.Model):
+    vehiculo = models.ForeignKey(Vehiculo, on_delete= models.CASCADE, blank=False, null=False)
+    venta = models.ForeignKey(Venta, on_delete= models.CASCADE, blank=False, null=False)
+    cantidad = models.IntegerField()
+    precio = models.BigIntegerField()
+
