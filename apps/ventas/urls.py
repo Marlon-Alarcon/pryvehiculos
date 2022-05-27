8b382e1671d613674a8871652d732cc2867efbcc
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2022-05-27 00:28:23 -0500

    Consultas

--- apps/ventas/urls.py
+++ apps/ventas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.ventas.views import listVentas, ventaCreate, ventaEdit, ventaElim, listVehiculoventa, VehiculoventaCreate, VehiculoventaEdit, VehiculoventaElim
+from apps.ventas.views import listVentas, ventaCreate, ventaEdit, ventaElim, listVehiculoventa, VehiculoventaCreate, VehiculoventaEdit, VehiculoventaElim, consulta1, consulta2, consulta3, consulta4
 
 app_name='ventas'
 urlpatterns = [
@@ -8,8 +8,15 @@ urlpatterns = [
     path('actualizar/<int:id_venta>/', ventaEdit, name='ventaEdit'),
     path('eliminar/<int:id_venta>/', ventaElim, name='ventaElim'),
 
+    #Venta Vehiculo
     path('VehiculoVent', listVehiculoventa, name='listVehiculoventa'),
     path('vehiculoventnuevo', VehiculoventaCreate, name='VehiculoventaCreate'),
     path('vehiventact/<int:id_vehiculoventa>/', VehiculoventaEdit, name='VehiculoventaEdit'),
     path('vehiventelim/<int:id_vehiculoventa>/', VehiculoventaElim, name='VehiculoventaElim'),
+
+    #Consultas
+    path('consulta/', consulta1, name='consulta1'),
+    path('consulta2/', consulta2, name='consulta2'),
+    path('consulta3/', consulta3, name='consulta3'),
+    path('consulta4/', consulta4, name='consulta4'),
 ]
--- apps/ventas/views.py
+++ apps/ventas/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from apps.ventas.models import Venta, VehiculoVenta
 from apps.ventas.form_Venta import VentaForm
 from apps.ventas.form_VehiculoVenta import vehiculoventaForm
+from django.db.models import Sum
 
 def listVentas(request):
     ventas = Venta.objects.all().order_by('id') # ordena de forma descendente por el id
     context = {'ventas':ventas} # creo una variable que se llama vehiculos: y luego a esa variable le asigno la lista de todos los vehiculos y esa se llama vehiculos, la que creé anteriormente
     #context contiene el listado de los vehiculos
     return render(request, 'ventas/listVentas.html', context) #aquí estoy renderizando, luego en '' pongo la pagina html que crearé para mostrar esta vista y luego le mando el contexto, que es context
+
+
 
 def home (request):
     return render(request, 'base/base.html')
@@ -104,3 +107,48 @@ def VehiculoventaElim(request, id_vehiculoventa):
         return redirect('ventas:listVehiculoventa')
     
     return render(request,'vehiculosventa/vehiculoventaElimform.html', {'vehiculoventa': vehiculoventa})
+
+
+
+
+#----------------------------------
+#------------ CONSULTAS -----------
+#----------------------------------
+
+def consulta1(request):
+    fechainit = request.POST.get('fechainit')
+    fechafin = request.POST.get('fechafin')
+    consulta1=Venta.objects.filter(fecha__range=[fechainit,fechafin]).aggregate(Sum('valorTotal'))
+    
+    context = {
+        'fechainit': fechainit,
+        'fechafin': fechafin,
+        'consulta1': consulta1,
+    }
+    print(consulta1)
+    return render(request,'consultas/primeraConsulta.html',{'context':context})
+
+
+def consulta2(request):
+    fechainit = request.POST.get('fechainit')
+    fechafin = request.POST.get('fechafin')
+  
+    consulta2=Venta.objects.values('fecha').filter(fecha__range=[fechainit,fechafin]).annotate(total=Sum('valorTotal'))
+    
+    return render(request,'consultas/segundaConsulta.html',{'consulta2':consulta2})
+
+def consulta3(request):
+    fechainit = request.POST.get('fechainit')
+    fechafin = request.POST.get('fechafin')
+  
+    consulta3=Venta.objects.values('tipoPago').filter(fecha__range=[fechainit,fechafin]).annotate(total=Sum('valorTotal'))
+    
+    return render(request,'consultas/terceraConsulta.html',{'consulta3':consulta3})
+
+
+def consulta4(request):
+    fechainit = request.POST.get('fechainit')
+    fechafin = request.POST.get('fechafin')
+  
+    consulta4=Venta.objects.values('user').filter(fecha__range=[fechainit,fechafin]).annotate(total=Sum('valorTotal'))
+    return render(request,'consultas/cuartaConsulta.html',{'consulta4':consulta4})
